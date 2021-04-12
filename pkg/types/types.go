fe44f757d657e663b308352a251fce3b8216f140
AmirjonQodirov <amirqodirov8383@gmail.com>
2021-04-12 12:48:33 +0500

    14/2

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -4,36 +4,38 @@ type Money int64
 
 type PaymentCategory string
 
-type PaymentStatus string
+type PaymentStatus string 
 
 type Currency string
 
-const (
-	PaymentStatusOk         PaymentStatus = "OK"
-	PaymentStatusFail       PaymentStatus = "FAIL"
-	PaymentStatusInProgress PaymentStatus = "INPROGRESS"
+
+const(
+	PaymentStatusOk	PaymentStatus="OK"
+	PaymentStatusFail PaymentStatus="FAIL"
+	PaymentStatusInProgress PaymentStatus="INPROGRESS"
+
 )
 
-type Payment struct {
-	ID        string
-	AccountID int64
-	Amount    Money
-	Category  PaymentCategory
-	Status    PaymentStatus
+type Payment struct{
+	ID 			string
+	AccountID 	int64
+	Amount 		Money
+	Category 	PaymentCategory
+	Status		PaymentStatus
 }
 
 type Phone string
 
-type Account struct {
-	ID      int64
-	Phone   Phone
+type Account struct{
+	ID int64
+	Phone Phone
 	Balance Money
 }
 
 type Favorite struct {
-	ID        string
-	AccountID int64
-	Name      string
-	Amount    Money
-	Category  PaymentCategory
+	ID 			string
+	AccountID 	int64
+	Name 		string
+	Amount 		Money
+	Category	PaymentCategory
 }
